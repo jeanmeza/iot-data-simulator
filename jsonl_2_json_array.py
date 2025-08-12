91944965afb44becfe17db69902480afb52c9b65
Jean Meza <jean.meza@manydesigns.com>
2025-08-12 17:11:13 +0200

    add comment to the python script and fix one of the json files

--- jsonl_2_json_array.py
+++ jsonl_2_json_array.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+This script reads a JSON Lines file and converts it into a JSON array format.
+The file does not necessarily have the `jsonl` extension, but it is structured as JSON Lines.
+"""
+
 input_file = "./data/signal-mongodb-07ago2025-userGDZ.json"
 output_file = "./data/07ago2025.json"
 
